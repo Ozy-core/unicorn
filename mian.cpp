a93292153e71fa7b5daaf6bb53413c87ff0a5088
Osier Nono <osiernono@gmail.com>
2025-11-02 21:14:07 -0600

    adding a way to light out the unicorns and texting out my throw and catch

--- mian.cpp
+++ mian.cpp
@@ -2,26 +2,28 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <stdexcept>
 
 using namespace std;
 
 class Unicorn
 {
-    private:
-        string name;
-        vector<string> uniName;
-    public:
+private:
+    string name;
+    static vector<string> uniName;
 
-    Unicorn ( const string& unicorn_name )
+public:
+    Unicorn(const string& unicorn_name)
     {
-
         auto it = find(uniName.begin(), uniName.end(), unicorn_name);
-        if ( it != uniName.end() )
+        if (it != uniName.end())
         {
             throw runtime_error("Unicorn name must be unique. This unicorn name already exists");
         }
+
         name = unicorn_name;
-        uniName.push_back(unicorn_name);      
+        uniName.push_back(unicorn_name);
+        cout << "Hello Unicorn: " << name << endl;
     }
 
     ~Unicorn()
@@ -30,23 +32,44 @@ class Unicorn
         cout << "Bye bye Unicorn: " << name << endl;
     }
 
-
+    static void listUnicorns()
+    {
+        if (uniName.empty())
+        {
+            cout << "No unicorns currently alive." << endl;
+        }
+        else
+        {
+            cout << "✨ Current unicorns: ";
+            for_each(uniName.begin(), uniName.end(), [](const string& n) {
+                cout << n << "\n";
+            });
+            cout << endl;
+        }
+    }
 };
+
+
+vector<string> Unicorn::uniName;
 
 int main()
 {
     try
     {
-        Unicorn u1("Twighlight");
-        Unicorn u2("Buttercup");
-        Unicorn u3("Rainbow Sparkles"); 
-        Unicorn u4("Buttercup");
+        Unicorn u1("Sparkle");
+        Unicorn::listUnicorns();
+
+        Unicorn u2("Rainbow");
+        Unicorn::listUnicorns();
+
+        // Throws here because "Sparkle" already exists
+        Unicorn u3("Sparkle");
+        Unicorn::listUnicorns();
     }
-    catch ( const exception& e )
+    catch (const exception& e)
     {
         cerr << "Exception: " << e.what() << endl;
     }
 
     return 0;
-
 }
